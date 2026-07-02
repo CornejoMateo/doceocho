6d700a3d2fc4fffd5b26d24849c0f7ca987c4b98
Mateo Cornejo <ccornejomateo@gmail.com>
2026-07-02 14:35:40 -0300

    Fix clearCache method

--- utils/cache.ts
+++ utils/cache.ts
@@ -1,7 +1,12 @@
-export const clearCache = () => {
+export const clearCache = async () => {
 	localStorage.clear();
 	if ('caches' in window) {
-		caches.keys().then((keys) => keys.forEach((key) => caches.delete(key)));
+		try {
+			const keys = await caches.keys();
+			await Promise.all(keys.map((key) => caches.delete(key)));
+		} catch (error) {
+			console.error('Failed to clear cache storage', error);
+		}
 	}
 	location.reload();
 };
