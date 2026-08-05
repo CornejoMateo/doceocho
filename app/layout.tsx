f5c8ce2ebc7cc6b2bfa1aa6ffbdaff55a32c8b6f
Mateo Cornejo <ccornejomateo@gmail.com>
2026-08-05 08:34:58 -0300

    CHange in viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,10 +37,7 @@ export default function RootLayout({
 				<meta name="apple-mobile-web-app-capable" content="yes" />
 				<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 				<meta name="apple-mobile-web-app-title" content="Doceocho" />
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes"
-				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 			</head>
 			<body className={`font-sans antialiased bg-background text-foreground min-h-screen`}>
 				<ThemeProvider
