765249a38e05446258e3f4711ed9a65704404ee7
Mateo Cornejo <ccornejomateo@gmail.com>
2026-09-18 16:40:01 -0300

    Enable Tab modules

--- components/business/clock-in/clock-in.tsx
+++ components/business/clock-in/clock-in.tsx
@@ -243,11 +243,7 @@ export function ClockIn() {
 				<Tabs defaultValue="hour" className="min-w-0">
 					<TabsList>
 						<TabsTrigger value="hour">Por hora</TabsTrigger>
-						{!isQR && (
-							<TabsTrigger value="module" disabled>
-								Por módulo
-							</TabsTrigger>
-						)}
+						{!isQR && <TabsTrigger value="module">Por módulo</TabsTrigger>}
 					</TabsList>
 
 					<TabsContent value="hour">
